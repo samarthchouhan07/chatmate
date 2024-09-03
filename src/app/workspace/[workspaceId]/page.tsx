7ed0e6c8b774115b9b742487a21b3693d9ba7b5c
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-09-04 00:24:40 +0530

    workspace creation done

---
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -0,0 +1,16 @@
+
+interface WorkspaceIdPageProps{
+    params:{
+        workspaceId:string
+    }
+}
+
+const WorkSpaceIdPage=({params}:WorkspaceIdPageProps)=>{
+ return (
+    <div>
+        ID: {params.workspaceId}
+    </div>
+ )
+}
+
+export default WorkSpaceIdPage
