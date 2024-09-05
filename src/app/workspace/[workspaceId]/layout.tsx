311eee2fb1aeb96a42da9bf29f076b54b9449092
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-09-05 21:10:44 +0530

    workspace header integrated

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,6 +1,13 @@
 "use client";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 interface WorkspaceidLayout {
   children: React.ReactNode;
@@ -11,8 +18,18 @@ const WorkspaceLayout = ({ children }: WorkspaceidLayout) => {
     <div className="h-full ">
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
-        <Sidebar/>
-        {children}
+        <Sidebar />
+        <ResizablePanelGroup direction="horizontal" autoSaveId="sa-workspace-layout">
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkspaceSidebar/>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   );
