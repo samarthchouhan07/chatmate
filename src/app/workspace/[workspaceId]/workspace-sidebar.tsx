db433332d9148438ec3ce189a5d97d47731c32b4
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-09-08 20:09:26 +0530

    channel page intergration started

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -15,11 +15,13 @@ import { WorkspaceSection } from "./workspace-section";
 import { useGetMembers } from "@/features/members/api/use-get-members";
 import { UserItem } from "./user-item";
 import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
+import { useChannelId } from "@/hooks/use-channel-id";
 
 export const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
+  const channelId = useChannelId();
 
-  const [_open,setOpen]=useCreateChannelModal();
+  const [_open, setOpen] = useCreateChannelModal();
   const { data: member, isLoading: memberLoading } = useCurrentMember({
     workspaceId,
   });
@@ -58,17 +60,26 @@ export const WorkspaceSidebar = () => {
         <SidebarItem label="Threads" icon={MessageSquareText} id="threads" />
         <SidebarItem label="Drafts & Sent" icon={SendHorizonal} id="drafts" />
       </div>
-      <WorkspaceSection label="Channels" hint="New Channel" onNew={member.role==="admin"?() =>setOpen(true):undefined}>
+      <WorkspaceSection
+        label="Channels"
+        hint="New Channel"
+        onNew={member.role === "admin" ? () => setOpen(true) : undefined}
+      >
         {channels?.map((item) => (
           <SidebarItem
             key={item._id}
             icon={HashIcon}
             label={item.name}
             id={item._id}
+            variant={channelId === item._id ? "active" : "default"}
           />
         ))}
       </WorkspaceSection>
-      <WorkspaceSection label="Direct Messages" hint="New direct message" onNew={() => {}}>
+      <WorkspaceSection
+        label="Direct Messages"
+        hint="New direct message"
+        onNew={() => {}}
+      >
         {members?.map((item) => (
           <UserItem
             key={item._id}
--- src/features/channels/components/create-channel-modal.tsx
+++ src/features/channels/components/create-channel-modal.tsx
@@ -12,8 +12,11 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useCreateChannel } from "../api/use-create-channel";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export const CreateChannelModal = () => {
+const router=useRouter()
   const workspaceId=useWorkspaceId()
   const [open, setOpen] = useCreateChannelModal();
   const [name,setName]=useState("")
@@ -32,7 +35,12 @@ export const CreateChannelModal = () => {
         name,workspaceId
     },{
         onSuccess:(id)=>{
+        toast.success("Channel created")
+            router.push(`/workspace/${workspaceId}/channel/${id}`)
             handleClose()
+        },
+        onError:()=>{
+          toast.error("Falied to create channel")
         }
     })
   }
