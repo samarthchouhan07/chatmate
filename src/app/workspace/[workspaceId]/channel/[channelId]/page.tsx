5877af7fc7cd575717528fe10a436b8d873f66af
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-09-08 23:28:19 +0530

    channel header integrated

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,5 +1,35 @@
+"use client"
+
+import { UseGetChannel } from "@/features/channels/api/use-get-channel";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { Loader, TriangleAlert } from "lucide-react";
+import { Header } from "./Header";
+
 const ChannelIdPage = () => {
-  return <div>Channel Id Page</div>;
+  const channelId =useChannelId()
+  const {data:channel,isLoading:channelLoading}=UseGetChannel({id:channelId})
+  if(channelLoading){
+    return (
+      <div className="h-full flex-1 flex items-center justify-center ">
+        <Loader className="animate-spin size-5 text-muted-foreground"/>
+      </div>
+    )
+  }
+
+  if(!channel){
+    return (
+      <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center ">
+        <TriangleAlert className="size-5 text-muted-foreground"/>
+        <span className="text-sm text-muted-foreground">
+          Channel not found
+        </span>
+      </div>
+    )
+  }
+
+  return <div className="flex flex-col h-full">
+    <Header title={channel.name}/>
+  </div>;
 };
 
 export default ChannelIdPage;
