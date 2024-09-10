c3e1ce91e748de4d9252b721429418aa861e49d1
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-09-10 22:23:05 +0530

    emoji picker component added

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -14,7 +14,8 @@ import { MdSend } from "react-icons/md";
 import { Hint } from "./hint";
 import { Delta, Op } from "quill/core";
 import { cn } from "@/lib/utils";
-import keyboard from "quill/modules/keyboard";
+import { EmojiPopover } from "./emoji-popover";
+
 
 type EditorValue = {
   image: File | null;
@@ -128,6 +129,12 @@ const Editor = ({
       toolbarElement.classList.toggle("hidden");
     }
   };
+
+  const onEmojiSelect=(emoji:any)=>{
+    const quill =quillRef.current
+    quill?.insertText(quill?.getSelection()?.index || 0, emoji.native)
+  }
+
   const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
   console.log({ isEmpty, text });
 
@@ -148,16 +155,15 @@ const Editor = ({
               <PiTextAa className="size-4" />
             </Button>
           </Hint>
-          <Hint label="Emoji">
+          <EmojiPopover onEmojiSelect={onEmojiSelect}>
             <Button
               disabled={disabled}
               size={"iconSm"}
               variant={"ghost"}
-              onClick={() => {}}
             >
               <Smile className="size-4" />
             </Button>
-          </Hint>
+          </EmojiPopover>
           {variant === "create" && (
             <Hint label="Image">
               <Button
@@ -207,11 +213,13 @@ const Editor = ({
           )}
         </div>
       </div>
-      <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
+      {variant==="create" && (
+        <div className={cn("p-2 text-[10px] text-muted-foreground flex justify-end opacity-0 transition",!isEmpty && "opacity-100")}>
         <p>
           <strong>Shift + Return</strong> to add a new line
         </p>
       </div>
+      )}
     </div>
   );
 };
