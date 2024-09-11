b7e6b807a0d849c2b226fcd2c7a01dc79bfb28d5
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-09-11 20:21:09 +0530

    Editor submit feature done

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,23 +1,62 @@
+import { useCreateMessage } from "@/features/messages/api/use-create-messsage";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import dynamic from "next/dynamic";
 import Quill from "quill";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { toast } from "sonner";
 
 const Editor = dynamic(() => import("@/components/Editor"), { ssr: false });
 
-interface ChatInputProps{
-    placeholder:string 
-
+interface ChatInputProps {
+  placeholder: string;
 }
 
-export const ChatInput = ({placeholder}:ChatInputProps) => {
+export const ChatInput = ({ placeholder }: ChatInputProps) => {
   const editorRef = useRef<Quill | null>(null);
+  const [editorKey, setEditorKey] = useState(0);
+  const [isPending, setIsPending] = useState(false);
+
+  const workspaceId = useWorkspaceId();
+  const channelId = useChannelId();
+
+  const { mutate: createMessage } = useCreateMessage();
+  const handleSubmit = async ({
+    body,
+    image,
+  }: {
+    body: string;
+    image: File | null;
+  }) => {
+    try {
+      setIsPending(true);
+
+      console.log({ body, image });
+      await createMessage(
+        {
+          workspaceId,
+          channelId,
+          body,
+        },
+        {
+          throwError: true,
+        }
+      );
+      setEditorKey((prevKey) => (prevKey = 1));
+    } catch (error) {
+      toast.error("Failed to send message");
+    } finally {
+      setIsPending(false);
+    }
+  };
 
   return (
     <div className="px-5 w-full">
       <Editor
+        key={editorKey}
         placeholder={placeholder}
-        onSubmit={() => {}}
-        disabled={false}
+        onSubmit={handleSubmit}
+        disabled={isPending}
         innerRef={editorRef}
       />
     </div>
